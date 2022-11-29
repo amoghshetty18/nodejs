b82a31fbd5c78a61daaf34b54e2b3dcc80ffcb5c
Amogh Shetty <virkohli30@gmail.com>
2022-11-29 13:21:12 +0000

    Module 4 pug

--- routes/admin.js
+++ routes/admin.js
@@ -7,17 +7,25 @@ const router = express.Router();
 
 //  /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  console.log("In another middleware");
   // res.send(
   //   '<form action="/admin/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
   // );
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
+
+const products = [];
 
 //  /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+module.exports = {
+  routes: router,
+  products: products,
+};
--- routes/shop.js
+++ routes/shop.js
@@ -6,12 +6,16 @@ const express = require("express");
 
 const router = express.Router();
 
+const adminData = require("./admin");
+
 router.get("/", (req, res, next) => {
-  console.log("In another middleware");
+  const { products } = adminData;
+  console.log(products);
   // res.send(
   //   "<html><head><title>Home Page</title></head><body><h1>hello from express.</h1><a href='/admin/add-product'>Add Product</a></body></html>"
   // );
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
